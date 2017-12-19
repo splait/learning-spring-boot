9dceeb2f33b4ed046902cc808d5e310708b247ab
gui314 <guisousa09@hotmail.com>
2017-12-18 23:53:57 -0200

    Changed @Autowire dependency injection in dateUtil field to constructor dependency injection; Added two new request methods.

--- src/main/java/learning/endpoint/StudentEndpoint.java
+++ src/main/java/learning/endpoint/StudentEndpoint.java
@@ -3,6 +3,9 @@ package learning.endpoint;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +21,31 @@ import java.time.LocalDateTime;
 @RequestMapping("student")
 public class StudentEndpoint {
 
+	private final DateUtil dateUtil;
+
 	@Autowired
-	private DateUtil dateUtil;
+	public StudentEndpoint(DateUtil dateUtil){
+		this.dateUtil = dateUtil;
+	}
 	
-	@RequestMapping(method= RequestMethod.GET, path="/list")
-	public List<Student> listAll(){
+	//@RequestMapping(method = RequestMethod.GET, path = "/list")
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> listAll() {
 		System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return asList(new Student("Guilherme"));
+		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="/{id}")
+	public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+		for(Student student: Student.studentList){
+			if(student.getId() == id)
+				return new ResponseEntity<>(student, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "hello")
+	public String hello() {
+		return "<html><body><p style=\"color: blue;\">Hello World!</p></body></html>";
 	}
 }
