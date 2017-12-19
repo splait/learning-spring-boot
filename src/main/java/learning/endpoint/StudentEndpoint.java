82b05baeaf837a0791d00440ef0a3709c63476b5
splait <guisousa09@hotmail.com>
2017-12-19 11:47:27 -0200

    Added POST, PUT and DELETE methods; Changed @RequestMapping to a more specific one like @GetMapping, @PostMapping, @PutMapping and @DeleteMapping in each request method

--- src/main/java/learning/endpoint/StudentEndpoint.java
+++ src/main/java/learning/endpoint/StudentEndpoint.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import learning.error.CustomErrorType;
 import learning.model.Student;
 import learning.util.DateUtil;
 
@@ -29,22 +35,48 @@ public class StudentEndpoint {
 	}
 	
 	//@RequestMapping(method = RequestMethod.GET, path = "/list")
-	@RequestMapping(method = RequestMethod.GET)
+	//@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<?> listAll() {
 		System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
 		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path="/{id}")
+	//@RequestMapping(method = RequestMethod.GET, path="/{id}")
+	@GetMapping(path="/{id}")
 	public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
-		for(Student student: Student.studentList){
-			if(student.getId() == id)
-				return new ResponseEntity<>(student, HttpStatus.OK);
-		}
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		Student student = new Student();
+		student.setId(id);
+		int index = Student.studentList.indexOf(student);
+		if(index == -1)
+			return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "hello")
+	//@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Student student) {
+		Student.studentList.add(student);
+		return new ResponseEntity<>(student, HttpStatus.OK);
+	}
+	
+	//@RequestMapping(method = RequestMethod.DELETE)
+	@DeleteMapping
+	public ResponseEntity<?> remove(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	//@RequestMapping(method = RequestMethod.PUT)
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		Student.studentList.add(student);
+		return new ResponseEntity<>(student, HttpStatus.OK);
+	}
+	
+	//@RequestMapping(method = RequestMethod.GET, path = "hello")
+	@GetMapping(path="/hello")
 	public String hello() {
 		return "<html><body><p style=\"color: blue;\">Hello World!</p></body></html>";
 	}
