1dd8868e3530fe11c47a33f9238d9e2711a4f51c
splait <guisousa09@hotmail.com>
2017-12-19 11:48:36 -0200

    Added CustomErrorType.java

---
+++ src/main/java/learning/error/CustomErrorType.java
@@ -0,0 +1,14 @@
+package learning.error;
+
+public class CustomErrorType {
+
+	private String errorMessage;
+	
+	public CustomErrorType(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
+	public String getErrorMessage(){
+		return this.errorMessage;
+	}
+}
